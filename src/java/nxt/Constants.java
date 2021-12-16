8911d47d8e1537c84750f8814f025c71e8bb2a6b
theBlue0x <3836882+theBlue0x@users.noreply.github.com>
2021-12-16 14:25:14 -0500

    update fees/block time

--- src/java/nxt/Constants.java
+++ src/java/nxt/Constants.java
@@ -36,7 +36,7 @@ public final class Constants {
     public static final long ONE_NXT = 100000000;
     public static final long MAX_BALANCE_NQT = MAX_BALANCE_NXT * ONE_NXT;
 
-    public static final int BLOCK_TIME = 25;
+    public static final int BLOCK_TIME = 20;
     public static final long INITIAL_BASE_TARGET = BigInteger.valueOf(2).pow(63).divide(BigInteger.valueOf(BLOCK_TIME * MAX_BALANCE_NXT)).longValue(); //153722867;
     public static final long MAX_BASE_TARGET = INITIAL_BASE_TARGET * (isTestnet ? MAX_BALANCE_NXT : 50);
     public static final long MIN_BASE_TARGET = INITIAL_BASE_TARGET * 9 / 10;
